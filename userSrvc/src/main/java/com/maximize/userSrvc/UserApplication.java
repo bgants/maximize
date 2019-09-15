d8cc5bf76e75857539a59aace01168425f9f54f7
bgants <bobgants@gmail.com>
2019-09-15 13:00:08 -0400

    working through client config for config server.

--- userSrvc/src/main/java/com/maximize/userSrvc/UserApplication.java
+++ userSrvc/src/main/java/com/maximize/userSrvc/UserApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
+@EnableConfigServer
 @SpringBootApplication
 public class UserApplication {
 

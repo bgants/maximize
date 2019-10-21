2bce42b22d2ab919c09c3453d895fb3c1a9ab5f1
bgants <bobgants@gmail.com>
2019-10-21 14:54:14 -0400

    adding registry services.

---
+++ discoverySrvc/src/main/java/com/maximize/discoverySrvc/DiscoverySrvcApplication.java
@@ -0,0 +1,15 @@
+package com.maximize.discoverySrvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverySrvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverySrvcApplication.class, args);
+	}
+
+}
---
+++ discoverySrvc/src/test/java/com/maximize/discoverySrvc/DiscoverySrvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maximize.discoverySrvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoverySrvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
